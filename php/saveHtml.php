d86b849e22595039b1d3f2eac19941a86da834db
AlexAvalosxP <AlexAvalosxP@users.noreply.github.com>
2018-06-20 00:12:41 -0500

    download Added

--- php/saveHtml.php
+++ php/saveHtml.php
@@ -2,9 +2,15 @@
 
 	$i = $_POST['i'];
 	$n = $_POST['n'];
+	$r = $_POST['r'];
 
 	$file = fopen('../documents/' . $n . '.txt','w+');
 	fwrite($file, $i);
 	fclose($file);
 
+/*
+	$file = fopen('../documents/' . $n . '.html','w+');
+	fwrite($file, "<html><head></head><body>" . $i . "</body></html>");
+	fclose($file);
+	*/
  ?>
